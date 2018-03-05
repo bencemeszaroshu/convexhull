01611ec6114a5abe70c1dd2c4537b09e257c1d6d
bencemeszaros <bence.meszaros@outlook.com>
2018-03-05 21:44:34 +0100

    Add Distance method to class Point

--- prezi/Point.h
+++ prezi/Point.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <math.h>
 class Point
 {
 public:
@@ -8,9 +9,13 @@ public:
     Point operator+(Point p) { return Point(x + p.x, y + p.y); };
     Point operator-(Point p) { return Point(x - p.x, y - p.y); };
     double operator*(Point p) { return x * p.x + y * p.y; };
+    bool operator==(Point p) { return x == p.x && y == p.y; };
+    double Cross(Point p) { return x * p.y - y * p.x; };
+    double Distance(Point p)
+    {
+        return sqrt((x - p.x) * (x - p.x) + (y - p.y) * (y - p.y));
+    }
     friend Point operator*(Point p, double mult) { return Point(p.x * mult, p.y * mult); };
     friend Point operator*(double mult, Point p) { return Point(p.x * mult, p.y * mult); };
-    double Cross(Point p) { return x * p.y - y * p.x; };
-    bool operator==(Point p) { return x == p.x && y == p.y; };
 };
 
