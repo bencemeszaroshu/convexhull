5dd2cf62a3b36674104057c707356244e204bac1
bencemeszaros <bence.meszaros@outlook.com>
2018-03-05 20:51:36 +0100

    Rename class Line to LineSegment and refactor

--- prezi/LineSegment.h
+++ prezi/LineSegment.h
@@ -2,30 +2,32 @@
 #include "Vector.h"
 #include <limits>
 
-class Line
+class LineSegment
 {
 public:
-    Line(Vector v1, Vector v2) :v1(v1), v2(v2) {};
-    Vector v1;
-    Vector v2;
-    Vector Intersects(Line l)
+    LineSegment(Vector start, Vector end) : start(start), end(end) {};
+    Vector start;
+    Vector end;
+    Vector Intersects(LineSegment line)
     {
-        Vector r = v2 - v1;
-        Vector s = l.v2 - l.v1;
+        Vector r = end - start;
+        Vector s = line.end - line.start;
+        Vector qp = line.start - start;
         double rxs = r.Cross(s);
-        double qpxr = (l.v1 - v1).Cross(r);
+        double qpxr = qp.Cross(r);
         bool collinear = rxs == 0 && qpxr == 0;
         bool parallel = rxs == 0 && qpxr != 0;
         if (collinear || parallel)
         {
             return Vector(std::nan(""), std::nan(""));
         }
-        double t = (l.v1 - v1).Cross(s) / rxs;
-        double u = (l.v1 - v1).Cross(r) / rxs;
+        double qpxs = qp.Cross(s);
+        double t = qpxs / rxs;
+        double u = qpxr / rxs;
         bool intersection = rxs != 0 && (0 <= t && t <= 1) && (0 <= u && u <= 1);
         if (intersection)
         {
-            return Vector(v1 + t*r);
+            return Vector(start + t*r);
         }
         return Vector(std::nan(""), std::nan(""));
     }
