076947297be50acefe780d69b8bb3c7493c5182d
bencemeszaros <bence.meszaros@outlook.com>
2018-03-05 20:55:46 +0100

    Rename a variable in class Vector, minor refactor

--- prezi/Vector.h
+++ prezi/Vector.h
@@ -5,12 +5,12 @@ public:
     Vector(double x1, double x2) : x1(x1), x2(x2) {};
     double x1;
     double x2;
-    Vector operator+(Vector y) { return Vector(x1 + y.x1, x2 + y.x2); };
-    Vector operator-(Vector y) { return Vector(x1 - y.x1, x2 - y.x2); };
-    double operator*(Vector y) { return x1 * y.x1 + x2 * y.x2; };
-    friend Vector operator*(Vector y, double mult) { return Vector(y.x1 * mult, y.x2 * mult); };
-    friend Vector operator*(double mult, Vector y) { return Vector(y.x1 * mult, y.x2 * mult); };
-    double Cross(Vector y) { return x1 * y.x2 - x2 * y.x1; };
-    bool operator==(Vector y) { return x1 == y.x1 && x2 == y.x2; };
+    Vector operator+(Vector v) { return Vector(x1 + v.x1, x2 + v.x2); };
+    Vector operator-(Vector v) { return Vector(x1 - v.x1, x2 - v.x2); };
+    double operator*(Vector v) { return x1 * v.x1 + x2 * v.x2; };
+    friend Vector operator*(Vector v, double mult) { return Vector(v.x1 * mult, v.x2 * mult); };
+    friend Vector operator*(double mult, Vector v) { return Vector(v.x1 * mult, v.x2 * mult); };
+    double Cross(Vector v) { return x1 * v.x2 - x2 * v.x1; };
+    bool operator==(Vector v) { return x1 == v.x1 && x2 == v.x2; };
 };
 
