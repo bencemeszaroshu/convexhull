9f1924d1e82f053d8e0a7ff8d57bfcb04716cc16
bencemeszaros <bence.meszaros@outlook.com>
2018-03-06 12:22:36 +0100

    Add new method Intersects(LineSegment lineSegment) to class Circle

--- prezi/Circle.cpp
+++ prezi/Circle.cpp
@@ -1,5 +1,7 @@
+#pragma once
 #include "stdafx.h"
 #include "Circle.h"
+#include <cmath>
 
 std::vector<Point> Circle::Intersects(Circle circle)
 {
@@ -13,7 +15,7 @@ std::vector<Point> Circle::Intersects(Circle circle)
     double base =
         ((radius * radius) - (circle.radius * circle.radius) + (distance * distance))
         / (2 * distance);
-    double height = sqrt(radius * radius - base * base);
+    double height = std::sqrt(radius * radius - base * base);
     Point p1 = (circle.center - center).Scale(base / distance) + center;
     if (distance == radius + circle.radius)
     {
@@ -26,5 +28,47 @@ std::vector<Point> Circle::Intersects(Circle circle)
         Point(p1.x + p2xh, p1.y + p2yh),
         Point(p1.x - p2xh, p1.y - p2yh)
     };
+    return intersections;
+}
+
+std::vector<Point> Circle::Intersects(LineSegment lineSegment)
+{
+    Point d = lineSegment.end - lineSegment.start;
+    Point f = lineSegment.start - center;
+    double a = d.Dot(d);
+    double b = 2 * f.Dot(d);
+    double c = f.Dot(f) - radius * radius;
+    double discriminant = b * b - 4 * a * c;
+    if (discriminant < 0)
+    {
+        return std::vector<Point>();
+    }
+    std::vector<Point> intersections;
+    if (discriminant == 0)
+    {
+        double t0 = -b / (2 * a);
+        if (t0 >= 0 && t0 <= 0)
+        {
+            Point p1 = lineSegment.start + d.Scale(t0);
+            intersections.push_back(p1);
+            return intersections;
+        }
+    }
+    else
+    {
+        discriminant = std::sqrt(discriminant);
+        double t1 = (-b - discriminant) / (2 * a);
+        if (t1 >= 0 && t1 <= 1)
+        {
+            Point p1 = lineSegment.start + d.Scale(t1);
+            intersections.push_back(p1);
+        }
+        double t2 = (-b + discriminant) / (2 * a);
+        if (t2 >= 0 && t2 <= 1)
+        {
+            Point p2 = lineSegment.start + d.Scale(t2);
+            intersections.push_back(p2);
+        }
+    }
     return intersections;
 }
--- prezi/Circle.h
+++ prezi/Circle.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "LineSegment.h"
 #include "Point.h"
 #include <vector>
 
@@ -7,6 +8,7 @@ class Circle
 public:
     Circle(Point center, double radius) : center(center), radius(radius) {};
     std::vector<Point> Intersects(Circle circle);
+    std::vector<Point> Intersects(LineSegment lineSegment);
     Point center;
     double radius;
 };
