a0b7c504f0fa6c8c3855db676d3b0e89cbe7bd43
bencemeszaros <bence.meszaros@outlook.com>
2018-03-06 20:42:25 +0100

    Add operator < to class Point and remove not needed semicolons

--- prezi/Point.h
+++ prezi/Point.h
@@ -4,19 +4,31 @@ class Point
 {
 public:
     Point(double x, double y) : x(x), y(y) {};
-    Point operator+(Point p) { return Point(x + p.x, y + p.y); };
-    Point operator-(Point p) { return Point(x - p.x, y - p.y); };
-    double operator*(Point p) { return x * p.x + y * p.y; };
-    bool operator==(Point p) { return x == p.x && y == p.y; };
-    double Cross(Point p) { return x * p.y - y * p.x; };
+    Point operator+(Point p) { return Point(x + p.x, y + p.y); }
+    Point operator-(Point p) { return Point(x - p.x, y - p.y); }
+    double operator*(Point p) { return x * p.x + y * p.y; }
+    bool operator==(Point p) { return x == p.x && y == p.y; }
+    bool operator<(Point p)
+    {
+        if (y != p.y)
+        {
+            return y < p.y;
+        }
+        return x < p.x;
+    }
+    double Cross(Point p) { return x * p.y - y * p.x; }
     double Distance(Point p)
     {
         return std::sqrt((x - p.x) * (x - p.x) + (y - p.y) * (y - p.y));
     }
-    double Dot(Point p) { return x * p.x + y * p.y; };
-    Point Scale(double s) { return Point(x * s, y * s); };
-    friend Point operator*(Point p, double mult) { return Point(p.x * mult, p.y * mult); };
-    friend Point operator*(double mult, Point p) { return Point(p.x * mult, p.y * mult); };
+    double DistanceSquare(Point p)
+    {
+        return (x - p.x) * (x - p.x) + (y - p.y) * (y - p.y);
+    }
+    double Dot(Point p) { return x * p.x + y * p.y; }
+    Point Scale(double s) { return Point(x * s, y * s); }
+    friend Point operator*(Point p, double mult) { return Point(p.x * mult, p.y * mult); }
+    friend Point operator*(double mult, Point p) { return Point(p.x * mult, p.y * mult); }
     double x;
     double y;
 };
