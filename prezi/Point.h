7d1f2bd73066a6a6e1f52ff6d2c1acba098b2757
bencemeszaros <bence.meszaros@outlook.com>
2018-03-06 22:06:53 +0100

    Make const parameters const in class Point

--- prezi/Point.h
+++ prezi/Point.h
@@ -4,11 +4,11 @@ class Point
 {
 public:
     Point(double x, double y) : x(x), y(y) {};
-    Point operator+(Point p) { return Point(x + p.x, y + p.y); }
-    Point operator-(Point p) { return Point(x - p.x, y - p.y); }
-    double operator*(Point p) { return x * p.x + y * p.y; }
-    bool operator==(Point p) { return x == p.x && y == p.y; }
-    bool operator<(Point p)
+    Point operator+(const Point& p) { return Point(x + p.x, y + p.y); }
+    Point operator-(const Point& p) { return Point(x - p.x, y - p.y); }
+    double operator*(const Point& p) { return x * p.x + y * p.y; }
+    bool operator==(const Point& p) { return x == p.x && y == p.y; }
+    bool operator<(const Point& p)
     {
         if (y != p.y)
         {
@@ -16,19 +16,19 @@ public:
         }
         return x < p.x;
     }
-    double Cross(Point p) { return x * p.y - y * p.x; }
-    double Distance(Point p)
+    double Cross(const Point& p) { return x * p.y - y * p.x; }
+    double Distance(const Point& p)
     {
         return std::sqrt((x - p.x) * (x - p.x) + (y - p.y) * (y - p.y));
     }
-    double DistanceSquare(Point p)
+    double DistanceSquare(const Point& p)
     {
         return (x - p.x) * (x - p.x) + (y - p.y) * (y - p.y);
     }
-    double Dot(Point p) { return x * p.x + y * p.y; }
+    double Dot(const Point& p) { return x * p.x + y * p.y; }
     Point Scale(double s) { return Point(x * s, y * s); }
-    friend Point operator*(Point p, double mult) { return Point(p.x * mult, p.y * mult); }
-    friend Point operator*(double mult, Point p) { return Point(p.x * mult, p.y * mult); }
+    friend Point operator*(const Point& p, double mult) { return Point(p.x * mult, p.y * mult); }
+    friend Point operator*(double mult, const Point& p) { return Point(p.x * mult, p.y * mult); }
     double x;
     double y;
 };
