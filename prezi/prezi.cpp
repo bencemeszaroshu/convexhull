cc0cba64a7d0266592479a94fbcb4e611672711c
bencemeszaros <bence.meszaros@outlook.com>
2018-03-06 15:06:48 +0100

    Add method to process input data

--- prezi/prezi.cpp
+++ prezi/prezi.cpp
@@ -2,19 +2,57 @@
 //
 
 #include "stdafx.h"
+#include "Circle.h"
 #include "LineSegment.h"
 #include "Point.h"
+#include <fstream>
 #include <iostream>
+
+void ProcessInput(std::istream& input, int K, std::vector<LineSegment>& lineSegments, 
+    std::vector<Circle>& circles);
 
 int main()
 {
-    LineSegment l1(Point(0, 0), Point(5, 5));
-    LineSegment l2(Point(0, 5), Point(5, 0));
-
-    auto v = l1.Intersects(l2);
-
-    std::cout << v.x << " " << v.y << std::endl;
-
-    return 0;
+    std::ifstream testInput;
+    testInput.open("D:\\Dokumentumok2\\prezi_hazi\\prezi\\Debug\\testinput.txt");
+    if (!testInput.is_open())
+    {
+        return 0;
+    }
+    int K;
+    testInput >> K;
+    std::vector<LineSegment> lineSegments;
+    lineSegments.reserve(K);
+    std::vector<Circle> circles;
+    circles.reserve(K);
+    ProcessInput(testInput, K, lineSegments, circles);
+    testInput.close();
+    int alma = 8;
 }
 
+void ProcessInput(std::istream& input, int K, std::vector<LineSegment>& lineSegments, std::vector<Circle>& circles)
+{
+    for (int i = 0; i < K; ++i)
+    {
+        char type;
+        input >> type;
+        if (type == 'C')
+        {
+            double x;
+            double y;
+            double radius;
+            input >> x >> y >> radius;
+            circles.push_back(Circle(Point(x, y), radius));
+
+        }
+        else if (type == 'L')
+        {
+            double x1;
+            double x2;
+            double y1;
+            double y2;
+            input >> x1 >> y1 >> x2 >> y2;
+            lineSegments.push_back(LineSegment(Point(x1, y1), Point(x2, y2)));
+        }
+    }
+}
