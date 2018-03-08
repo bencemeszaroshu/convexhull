888517fb2087cfa0ddc9090edd20891b30ec972f
bencemeszaros <bence.meszaros@outlook.com>
2018-03-08 01:32:12 +0100

    Add const where appropriate in class Point

--- prezi/Point.cpp
+++ prezi/Point.cpp
@@ -1,7 +1,7 @@
 #include "stdafx.h"
 #include "Point.h"
 
-bool Point::operator<(const Point& p)
+bool Point::operator<(const Point& p) const
 {
     if (y != p.y)
     {
@@ -10,7 +10,7 @@ bool Point::operator<(const Point& p)
     return x < p.x;
 }
 
-int Point::ClockWiseTurn(const Point& p1, const Point& p2)
+int Point::ClockWiseTurn(const Point& p1, const Point& p2) const
 {
     double area = (p1.x - x) * (p2.y - y) - (p1.y - y) * (p2.x - x);
     if (area > 0)
@@ -24,12 +24,12 @@ int Point::ClockWiseTurn(const Point& p1, const Point& p2)
     return 0;
 }
 
-double Point::Distance(const Point& p)
+double Point::Distance(const Point& p) const
 {
     return std::sqrt((x - p.x) * (x - p.x) + (y - p.y) * (y - p.y));
 }
 
-double Point::DistanceSquare(const Point& p)
+double Point::DistanceSquare(const Point& p) const
 {
     return (x - p.x) * (x - p.x) + (y - p.y) * (y - p.y);
 }
--- prezi/Point.h
+++ prezi/Point.h
@@ -6,17 +6,17 @@ class Point
 {
 public:
     Point(double x, double y) : x(x), y(y) {};
-    Point operator+(const Point& p) { return Point(x + p.x, y + p.y); }
-    Point operator-(const Point& p) { return Point(x - p.x, y - p.y); }
-    double operator*(const Point& p) { return x * p.x + y * p.y; }
+    Point operator+(const Point& p) const { return Point(x + p.x, y + p.y); }
+    Point operator-(const Point& p) const { return Point(x - p.x, y - p.y); }
+    double operator*(const Point& p) const { return x * p.x + y * p.y; }
     bool operator==(const Point& p) const { return x == p.x && y == p.y; }
-    bool operator<(const Point& p);
+    bool operator<(const Point& p) const;
     double Cross(const Point& p) { return x * p.y - y * p.x; }
-    int ClockWiseTurn(const Point& p1, const Point& p2);
-    double Distance(const Point& p);
-    double DistanceSquare(const Point& p);
-    double Dot(const Point& p) { return x * p.x + y * p.y; }
-    Point Scale(double s) { return Point(x * s, y * s); }
+    int ClockWiseTurn(const Point& p1, const Point& p2) const;
+    double Distance(const Point& p) const;
+    double DistanceSquare(const Point& p) const;
+    double Dot(const Point& p) const { return x * p.x + y * p.y; }
+    Point Scale(double s) const { return Point(x * s, y * s); }
     friend Point operator*(const Point& p, double mult) { return Point(p.x * mult, p.y * mult); }
     friend Point operator*(double mult, const Point& p) { return Point(p.x * mult, p.y * mult); }
     friend std::ostream& operator<<(std::ostream& os, const Point& p)
